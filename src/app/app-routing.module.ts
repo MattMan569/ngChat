26ba7ce99434517c63ad5ac7e471f9fc9ea37612
MattMan569 <matthewpolsom@hotmail.com>
2020-04-08 16:46:35 -0600

    add rooms route, remove preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/rooms', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'rooms', loadChildren: () => import('./rooms/rooms.module').then(m => m.RoomsModule) },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
-    }),
+    RouterModule.forRoot(routes),
   ],
   exports: [RouterModule],
 })

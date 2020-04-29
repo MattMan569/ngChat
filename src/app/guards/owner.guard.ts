e4481dc50e7dc297abe4422d4b181934cfcc3317
MattMan569 <matthewpolsom@hotmail.com>
2020-04-29 14:05:07 -0600

    room owner guard for room edit

---
+++ src/app/guards/owner.guard.ts
@@ -0,0 +1,30 @@
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+import { RoomService } from '../services/room.service';
+import IRoom from 'types/room';
+
+@Injectable()
+export class OwnerGuard implements CanActivate {
+  constructor(private authService: AuthService, private roomService: RoomService, private router: Router) { }
+
+  // Only allow the owner of the room to access its configuration page
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    const userId = this.authService.getUserId();
+    const roomId = state.url.split('/').pop();
+
+    const allowed = new Promise<boolean>(async (resolve, reject) => {
+      const room = await (this.roomService.getRoom(roomId).pipe(first()).toPromise() as Promise<IRoom>);
+      if (room.owner === userId) {
+        resolve(true);
+      } else {
+        this.router.navigate(['/rooms']);
+        resolve(false);
+      }
+    });
+
+    return allowed;
+  }
+}
--- src/app/rooms/rooms-routing.module.ts
+++ src/app/rooms/rooms-routing.module.ts
@@ -3,17 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RoomsComponent } from './rooms.component';
 import { AuthGuard } from './../guards/auth.guard';
+import { OwnerGuard } from '../guards/owner.guard';
 
 const routes: Routes = [
   { path: '', component: RoomsComponent },
   { path: 'create', loadChildren: () => import('./create/create.module').then(m => m.CreateModule), canActivate: [AuthGuard] },
-  { path: 'edit/:id', loadChildren: () => import('./create/create.module').then(m => m.CreateModule), canActivate: [AuthGuard] },
+  {
+    path: 'edit/:id',
+    loadChildren: () => import('./create/create.module').then(m => m.CreateModule),
+    canActivate: [AuthGuard, OwnerGuard],
+  },
   { path: 'chat/:id', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule), canActivate: [AuthGuard] },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [AuthGuard],
+  providers: [AuthGuard, OwnerGuard],
 })
 export class RoomsRoutingModule { }

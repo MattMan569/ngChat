39a869929d717adf3d218509ff55bc2c9f6280f1
MattMan569 <matthewpolsom@hotmail.com>
2020-06-01 20:16:38 -0600

    basic profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/rooms', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'rooms', loadChildren: () => import('./rooms/rooms.module').then(m => m.RoomsModule) },
+  // TODO query param for other user's profile
+  { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
 ];
 
 @NgModule({
---
+++ src/app/profile/profile.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileComponent } from './profile.component';
+import { AngularMaterialModule } from '../angular-material.module';
+
+
+@NgModule({
+  declarations: [ProfileComponent],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    AngularMaterialModule,
+  ],
+})
+export class ProfileModule { }

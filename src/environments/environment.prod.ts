50b6f066749338ea1dc83d967223c3ab9c41a233
MattMan569 <matthewpolsom@hotmail.com>
2020-08-06 20:05:15 -0600

    add prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  autoRefreshTokenTimeMargin: 10000,
+  apiUrl: 'https://polsom-ngchat-server.herokuapp.com/api',
   // TODO deployment
   // apiUrl:
 };
